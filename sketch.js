4cec86e51d9a6d842b33329d6db27fc8bea41c1a
Goose <117827921+Goose184@users.noreply.github.com>
2023-02-03 14:03:29 +0000

    Add files via upload
    p5 draw sketch for github

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(400, 400);
+  background(120);
+}
+
+function draw(){
+
+
+
+}
+
+function mouseDragged(){
+  noStroke()
+  fill(220, 19, 80)
+  circle(mouseX, mouseY, 20)
+  
+}
